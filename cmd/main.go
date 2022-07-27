d0655418a9d9c30990e1e73494cf00ef0e8b6d0a
mortx <mortxbox@live.com>
2022-07-27 16:08:10 +0100

    make runner

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"github.com/rs/zerolog/log"
+	"os"
 )
 
 func main() {
+	if os.Getenv("PRODUCTION") != "" {
+		log.Info().Msg("I'm in production")
+	}
 	log.Info().Msg("This is a message!")
 	log.Error().Msg("This is an error message")
 }
